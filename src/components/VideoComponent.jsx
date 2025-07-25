b60a1d92413781087e57fbc04b8cd1c1422dc980
PinoqioDOM <alkhanishvilit@gmail.com>
2025-07-25 17:57:50 +0400

    Video Bug fix

--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -3,7 +3,7 @@ import MyVideo from '../assets/Matrix.mp4'
 const VideoComponent = () => {
   return (
     <div>
-        <video src={MyVideo} autoPlay muted loop className='video-background'></video> 
+        <video src={MyVideo} autoPlay muted loop playsInline className='video-background'></video> 
     </div>
   )
 }
